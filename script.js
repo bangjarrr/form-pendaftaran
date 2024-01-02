6199b6e4f415aa36befb11aead35796c7698f7a9
bangjarrr <alfajjar16@gmail.com>
2024-01-02 20:48:14 +0700

    a

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const myForms = document.querySelectorAll('.form');
 function kosongFormulirAlert() {
     disableForm();
     alert.style.display = "block";
-    dataUtama.style.position = 'relative';
-    dataPelengkap.style.position = "relative";
 }
 
 function resetForm() {
@@ -56,4 +54,50 @@ function uncheckOtherCheckboxes(checkedCheckbox) {
         }
     });
 }
+
+
+function sendToWhatsApp() {
+    
+    try {
+        var nomorWhatsApp = '6281615650796';
+        var nama = document.getElementById('nama').value;
+        var tempatLahir = document.getElementById('tempatLahir').value;
+        var tglLahir = document.getElementById('tanggalLahir').value;
+        var jenisKelamin = document.querySelector('input[name="jenisKelamin"]:checked').value;
+        var asalSekolah = document.getElementById('asalSekolah').value;
+        var nomorHp = document.getElementById('nomorHp').value;
+        var alamat = document.getElementById('alamat').value;
+        var namaOrtu = document.getElementById('namaOrtu').value;
+        var pekerjaanOrtu = document.getElementById('pekerjaan').value;
+        var alamatOrtu = document.getElementById('alamat-ortu').value; 
+
+        if (!nama || !tempatLahir || !tglLahir || !jenisKelamin || !asalSekolah || !nomorHp || !alamat || !namaOrtu || !pekerjaanOrtu || !alamatOrtu ) {
+            return;
+        }
+        
+        // Tautan WhatsApp 
+        var linkWhatsApp = 'https://wa.me/' + nomorWhatsApp +
+                           '?text=Nama : %20' + encodeURIComponent(nama) +
+                           '%0ATempat lahir : %20' + encodeURIComponent(tempatLahir) + 
+                           '%0ATanggal lahir : %20' + encodeURIComponent(tglLahir) +
+                           '%0AJenis Kelamin : %20' + encodeURIComponent(jenisKelamin) +
+                           '%0Asal Sekolah : %20' + encodeURIComponent(asalSekolah) +
+                           '%0ANomor HP : %20' + encodeURIComponent(nomorHp) +
+                           '%0AAlamat : %20' + encodeURIComponent(alamat) +
+                           '%0ANama Orangtua : %20' + encodeURIComponent(namaOrtu) +
+                           '%0APekerjaan Orangtua : %20' + encodeURIComponent(pekerjaanOrtu) +
+                           '%0AAlamat Orangtua : %20' + encodeURIComponent(alamatOrtu);
+
+                           
+        window.open(linkWhatsApp, '_blank');
+    } catch (error) {
+        
+        // Menangkap dan menanggapi kesalahan
+        alert('Terjadi kesalahan: ' + error.message);
+    }
+}
+const btnKirim = document.getElementById('submit');
+
+btnKirim.addEventListener("click", sendToWhatsApp);
+
 
